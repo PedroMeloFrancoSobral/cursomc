b961c551ffc31b77bf399c4c47c9d9a073bfffc3
Pedro Sobral <pedrosobral2009@gmail.com>
2022-10-17 20:36:22 -0300

    Projeto Criado

---
+++ src/test/java/com/pedrosobral/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosobral.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
