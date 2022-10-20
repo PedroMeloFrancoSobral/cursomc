287ff50ca6e15fe64363cfc3015e3eb3e277ebe6
Pedro Sobral <pedrosobral2009@gmail.com>
2022-10-19 21:58:27 -0300

    estado e cidade

--- src/main/java/com/pedrosobral/cursomc/CursomcApplication.java
+++ src/main/java/com/pedrosobral/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pedrosobral.cursomc.domain.Categoria;
+import com.pedrosobral.cursomc.domain.Cidade;
+import com.pedrosobral.cursomc.domain.Estado;
 import com.pedrosobral.cursomc.domain.Produto;
 import com.pedrosobral.cursomc.repositories.CategoriaRepository;
+import com.pedrosobral.cursomc.repositories.CidadeRepository;
+import com.pedrosobral.cursomc.repositories.EstadoRepository;
 import com.pedrosobral.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,11 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepostory;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,5 +49,20 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		categoriaRepostory.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		Estado est1 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Minas Gerais");
+		
+		Cidade cid1 = new Cidade(null,"Uberlândia",est2);
+		Cidade cid2 = new Cidade(null,"São Paulo", est1);
+		Cidade cid3 = new Cidade(null,"Campinas",est1);
+		
+		est1.getCidades().addAll(Arrays.asList(cid2,cid3));
+		est2.getCidades().addAll(Arrays.asList(cid1));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
+		
 	}
 }
