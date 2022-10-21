bb102beadcf0a275c38efc918c3ff2cba0207aaa
Pedro Sobral <pedrosobral2009@gmail.com>
2022-10-20 21:11:48 -0300

    Endpoint/clientes/{id} disponivel

--- src/main/java/com/pedrosobral/cursomc/domain/Cidade.java
+++ src/main/java/com/pedrosobral/cursomc/domain/Cidade.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade {
 	
@@ -16,6 +18,8 @@ public class Cidade {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
--- src/main/java/com/pedrosobral/cursomc/domain/Estado.java
+++ src/main/java/com/pedrosobral/cursomc/domain/Estado.java
@@ -10,12 +10,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@JsonBackReference
 	@OneToMany(mappedBy="estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
