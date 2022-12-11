9ef7ab8bbc6c8fd70872139ddedb559a45e9c6d4
Pedro Sobral <pedrosobral2009@gmail.com>
2022-12-11 09:22:11 -0300

    Deletando uma Categoria com DELETE e error Handler

--- src/main/java/com/pedrosobral/cursomc/services/CategoriaService.java
+++ src/main/java/com/pedrosobral/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.pedrosobral.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pedrosobral.cursomc.domain.Categoria;
 import com.pedrosobral.cursomc.repositories.CategoriaRepository;
+import com.pedrosobral.cursomc.services.exceptions.DataIntegrityException;
 import com.pedrosobral.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -26,5 +28,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir categoria pois a mesma contém produtos vinculados");
+		}
 	}
 }
