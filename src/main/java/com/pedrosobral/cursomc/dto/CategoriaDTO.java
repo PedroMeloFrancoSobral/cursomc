ee7b870ff2a54c19e52fb5c6275bd1e63b90a9df
Pedro Sobral <pedrosobral2009@gmail.com>
2022-12-11 14:54:55 -0300

    Validacao sintatica com bean validation

--- src/main/java/com/pedrosobral/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/pedrosobral/cursomc/dto/CategoriaDTO.java
@@ -2,11 +2,18 @@ package com.pedrosobral.cursomc.dto;
 
 import java.io.Serializable;
 
-import com.pedrosobral.cursomc.domain.Categoria;
+import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.Length;
+
+import com.pedrosobral.cursomc.domain.Categoria;
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
+	@NotEmpty (message="Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message="O tamanho deve ser entre 5 a 80 caracteres")
+	
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/pedrosobral/cursomc/services/CategoriaService.java
+++ src/main/java/com/pedrosobral/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pedrosobral.cursomc.domain.Categoria;
+import com.pedrosobral.cursomc.dto.CategoriaDTO;
 import com.pedrosobral.cursomc.repositories.CategoriaRepository;
 import com.pedrosobral.cursomc.services.exceptions.DataIntegrityException;
 import com.pedrosobral.cursomc.services.exceptions.ObjectNotFoundException;
@@ -49,5 +50,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
