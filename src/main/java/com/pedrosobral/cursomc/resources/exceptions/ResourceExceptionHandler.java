c4545d35fdb020bbe90bed12b1c80d55e2648d87
Pedro Sobral <pedrosobral2009@gmail.com>
2022-12-11 15:12:30 -0300

    Personalisando o retorno da validacao

--- src/main/java/com/pedrosobral/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/pedrosobral/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,12 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.pedrosobral.cursomc.services.exceptions.DataIntegrityException;
 import com.pedrosobral.cursomc.services.exceptions.ObjectNotFoundException;
-
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class)
@@ -20,6 +21,15 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrityException(DataIntegrityException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> MethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		for (FieldError x: e.getBindingResult().getFieldErrors()) {
+			err.AddError(x.getField(), x.getDefaultMessage());
+		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
